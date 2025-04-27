73a40d05f767a3ebd7b9f47dae80862638f7a4a4
OGDogcat2 <wicker.noah@marblefallsisd.org>
2025-04-27 17:17:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,66 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-let players = {};
+const lobbies = {}; // { lobbyId: { players: {} } }
 
-io.on('connection', (socket) => {
-  players[socket.id] = { x: 0, z: 0, health: 100, name: "Player" + Math.floor(Math.random()*1000) };
+io.on('connection', socket => {
+  console.log(`User connected: ${socket.id}`);
 
-  socket.on('move', (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x += data.x * 0.1;
-      players[socket.id].z += data.z * 0.1;
-    }
-    io.emit('updatePlayers', players);
+  socket.on('createLobby', () => {
+    const lobbyId = uuidv4().slice(0, 5); // Shorter Lobby ID
+    lobbies[lobbyId] = { players: {} };
+    socket.join(lobbyId);
+    socket.lobbyId = lobbyId;
+    lobbies[lobbyId].players[socket.id] = { x: 0, z: 0 };
+    socket.emit('lobbyCreated', { lobbyId });
+    console.log(`Lobby created: ${lobbyId}`);
   });
 
-  socket.on('shoot', () => {
-    for (let id in players) {
-      if (id !== socket.id) {
-        let dx = players[socket.id].x - players[id].x;
-        let dz = players[socket.id].z - players[id].z;
-        if (Math.sqrt(dx*dx + dz*dz) < 5) {
-          players[id].health -= 10;
-          if (players[id].health <= 0) {
-            players[id].health = 100;
-            players[id].x = Math.random()*100-50;
-            players[id].z = Math.random()*100-50;
-          }
-        }
-      }
+  socket.on('joinLobby', ({ lobbyId }) => {
+    if (lobbies[lobbyId]) {
+      socket.join(lobbyId);
+      socket.lobbyId = lobbyId;
+      lobbies[lobbyId].players[socket.id] = { x: 0, z: 0 };
+      socket.emit('joinedLobby', { lobbyId });
+      console.log(`User ${socket.id} joined lobby ${lobbyId}`);
+    } else {
+      socket.emit('error', 'Lobby does not exist.');
+    }
+  });
+
+  socket.on('move', move => {
+    const lobbyId = socket.lobbyId;
+    if (lobbyId && lobbies[lobbyId] && lobbies[lobbyId].players[socket.id]) {
+      const player = lobbies[lobbyId].players[socket.id];
+      player.x += move.x * 0.1;
+      player.z += move.z * 0.1;
+
+      io.to(lobbyId).emit('updatePlayers', lobbies[lobbyId].players);
     }
   });
 
   socket.on('disconnect', () => {
-    delete players[socket.id];
-    io.emit('updatePlayers', players);
+    const lobbyId = socket.lobbyId;
+    if (lobbyId && lobbies[lobbyId]) {
+      delete lobbies[lobbyId].players[socket.id];
+      if (Object.keys(lobbies[lobbyId].players).length === 0) {
+        delete lobbies[lobbyId];
+        console.log(`Lobby ${lobbyId} deleted`);
+      }
+    }
+    console.log(`User disconnected: ${socket.id}`);
   });
 });
 
-http.listen(3000, () => {
-  console.log('Server started on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running...');
 });
+
